60536db77bb690e0087dd3640542717f0a8bad43
Tadasei <chemselhak.mahouche@gmail.com>
2024-04-11 13:42:16 +0100

    fixed migration naming

--- stubs/database/migrations/2023_05_13_195531_create_files_table.php
+++ stubs/database/migrations/2023_05_13_195531_create_files_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
 	 */
 	public function up(): void
 	{
-		Schema::create("fileables", function (Blueprint $table) {
+		Schema::create("files", function (Blueprint $table) {
 			$table->id();
 			$table->string("name");
 			$table->string("path")->unique();
