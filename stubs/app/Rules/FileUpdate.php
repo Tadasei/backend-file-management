4a20c810ada8681be0d48fc74302743c901e7208
Tadasei <chemselhak.mahouche@gmail.com>
2024-08-22 13:16:40 +0100

    added mime type, max file size, records filtering and uuid support to FileUpdate validation rule

--- stubs/app/Rules/FileUpdate.php
+++ stubs/app/Rules/FileUpdate.php
@@ -5,10 +5,19 @@ namespace App\Rules;
 use App\Models\File;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\UploadedFile;
 
 class FileUpdate implements ValidationRule
 {
+	public function __construct(
+		protected ?array $allowedMimeTypes = null,
+		protected ?int $maxFileSize = null,
+		protected ?Closure $recordsFilteringQueryFactory = null,
+		protected bool $usesUuid = false
+	) {
+	}
+
 	/**
 	 * Run the validation rule.
 	 *
@@ -21,11 +30,46 @@ class FileUpdate implements ValidationRule
 	): void {
 		if (
 			!(
-				$value instanceof UploadedFile ||
-				(is_numeric($value) && File::where("id", $value)->exists())
+				$this->isAValidFileId($value) ||
+				($value instanceof UploadedFile &&
+					$this->hasValidFileSize($value) &&
+					$this->hasValidMimeType($value))
 			)
 		) {
 			$fail(__("Invalid $attribute"));
 		}
+	}
+
+	private function hasValidFileSize(UploadedFile $file): bool
+	{
+		return is_null($this->maxFileSize) ||
+			$file->getSize() / 1024 <= $this->maxFileSize;
+	}
+
+	private function hasValidMimeType(UploadedFile $file): bool
+	{
+		return is_null($this->allowedMimeTypes) ||
+			in_array($file->getMimeType(), $this->allowedMimeTypes);
+	}
+
+	private function hasValidIdDataType(mixed $value): bool
+	{
+		return $this->usesUuid ? is_string($value) : is_int($value);
+	}
+
+	private function isAValidFileId(mixed $value): bool
+	{
+		return $this->hasValidIdDataType($value) &&
+			File::when(
+				$this->recordsFilteringQueryFactory,
+				fn(Builder $query) => $query->whereIn(
+					"id",
+					call_user_func($this->recordsFilteringQueryFactory)->select(
+						"id"
+					)
+				)
+			)
+				->where("id", $value)
+				->exists();
 	}
 }
